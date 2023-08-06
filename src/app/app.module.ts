5c97358f7481d90103901287ec3483d1d870ebf9
Matias Pellegrino <matiaspellegrino@outlook.com>
2023-08-06 18:40:00 -0300

    Agrego cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { IntroComponent } from './intro/intro.component';
+import { FooterComponent } from './footer/footer.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { EquipoComponent } from './equipo/equipo.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    IntroComponent,
+    FooterComponent,
+    NosotrosComponent,
+    ServiciosComponent,
+    PortfolioComponent,
+    EquipoComponent,
+    ClientesComponent,
+    BlogComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
